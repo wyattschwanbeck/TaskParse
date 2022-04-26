ac7cdb9ae9c1a2ccc415fd755856f471f0a2ac1d
Wyatt Schwanbeck <wyattschwanbeck@gmail.com>
2022-04-25 22:17:09 -0400

    Add project files.

---
+++ TaskParse/TaskSchedulerSearch.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TaskParse
+{
+    partial class TaskSchedulerSearch
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search Tasks";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(26, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(725, 304);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Task Scheduler Results";
+            // 
+            // TaskSchedulerSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "TaskSchedulerSearch";
+            this.Text = "Scheduled Task Parse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ TaskParse/TaskSchedulerSearch.cs
@@ -0,0 +1,93 @@
+﻿//using Microsoft.Win32.TaskScheduler;
+using Microsoft.Win32.TaskScheduler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using TaskScheduler;
+
+namespace TaskParse
+{
+    public partial class TaskSchedulerSearch : Form
+    {
+        private List<Regex> ReleventActions;
+        private string _searchTerm;
+        private Regex WindowsFileMatch;
+        public TaskSchedulerSearch()
+        {
+            ReleventActions = new List<Regex>();
+            InitializeComponent();
+
+            //R file search in tasks
+            ReleventActions.Add(new Regex(@"[a-zA-Z]:[\\\/](?:[a-zA-Z0-9]+[\\\/])*([a-zA-Z0-9]+\.R)"));
+
+            //VBS file search in tasks
+            ReleventActions.Add(new Regex(@"[a-zA-Z]:[\\\/](?:[a-zA-Z0-9]+[\\\/])*([a-zA-Z0-9]+\.vbs)"));
+
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            this._searchTerm = textBox1.Text;
+            using (TaskService ts = new TaskService())
+            {
+                
+                foreach (Task task in ts.AllTasks)
+                {
+                    //this.listBox1.Items.Add($"Parsing {task.Name}");
+                    CheckTask(task.Definition);
+                    
+                    this.Invalidate();
+                }
+            }
+                
+        }
+
+     private void CheckTask(TaskDefinition TD)
+        {
+            ActionCollection actions = TD.Actions;
+            
+            foreach(Microsoft.Win32.TaskScheduler.Action action in actions)
+            {
+                foreach(Regex releventAction in ReleventActions)
+                {
+                    MatchCollection matches = releventAction.Matches(action.ToString());
+                    if (matches.Count > 0)
+                        ParseActionFile(matches.First());
+                }
+                //this.listBox1.Items.Add(action.ToString());
+                
+            }
+        }
+        private void ParseActionFile(Match matchedActionStr)
+        {
+            try
+            {
+                //Parse text file for matched text
+                string[] parsedLines = File.ReadAllLines(matchedActionStr.Value);
+                foreach(string line in parsedLines)
+                {
+                    if (line.Contains(this._searchTerm))
+                    {
+                        this.listBox1.Items.Add(matchedActionStr.Value);
+                    }
+                }
+
+            }
+            catch (IOException)
+            {
+                //Try to parse SAS Project
+
+            }
+        }
+    }
+
+}
